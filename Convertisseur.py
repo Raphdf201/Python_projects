2f9e7afceec6980fb3bfd85d8d1b4dbbc03e1adf
Raphdf201 <raphdf201@hotmail.com>
2023-10-19 08:17:51 -0400

    little corrections

--- Convertisseur.py
+++ Convertisseur.py
@@ -1,24 +1,36 @@
 def longueur():
     print("Convertisseur d'unités de mesure de longueur")
 
+
 def energie():
     print("Convertisseur d'unités de mesure d'énergie")
 
+
 def vitesse():
     print("Convertisseur d'unités de vitesse")
+
 
 def surprise():
     print("UWU")
     exit()
 
+
 def depart():
     print("Bienvenue dans mon convertisseur d'unités")
-    departInput = input("Pour convertir une unité de longueur, écrire L, pour l'énergie, E et pour quitter, Q")
-    if departInput == "L":longueur()
-    elif departInput == "E":energie()
-    elif departInput == "V":vitesse()
-    elif departInput == "EASTEREGG":surprise()
-    elif departInput == "Q":exit()
-    else:print("Veuillez entrer une lettre valide")
+    departInput = input(
+        "Pour convertir une unité de longueur, écrire L, pour l'énergie, E et Q pour quitter : ")
+    if departInput == "L":
+        longueur()
+    elif departInput == "E":
+        energie()
+    elif departInput == "V":
+        vitesse()
+    elif departInput == "EASTEREGG":
+        surprise()
+    elif departInput == "Q":
+        exit()
+    else:
+        print("Veuillez entrer une lettre valide")
+
 
 depart()
