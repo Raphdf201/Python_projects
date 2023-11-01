a44967031367cfe2f4e4fb3b1d8d195a5b2faf1c
Raphdf201 <raphdf201@hotmail.com>
2023-11-01 09:46:07 -0400

    main terminé

--- main.py
+++ main.py
@@ -6,3 +6,31 @@ import manoir_hanté
 import tests
 import x3
 
+print("Quel projet voulez-vous lancer ? (entrer les lettre correspondante)")
+print("Calculatrice : cal")
+print("Convertisseur : conv")
+print("Dé : de")
+print("Générateur de mot de passe : mdp")
+print("Jeu de manoir hanté : mh")
+print("Fichier de tests : test")
+print("Jeu de 3 : x3")
+
+quel = input()
+
+if quel == "cal":
+    Calcultrice.launch_cal
+elif quel == "conv":
+    Convertisseur.launch_cv
+elif quel == "de":
+    dé.dé
+elif quel == "mdp":
+    generateur_mdp.launch_mdp
+elif quel == "mh":
+    manoir_hanté.manoirHanté
+elif quel == "test":
+    tests.launch_tests
+elif quel == "x3":
+    x3.x3
+else:
+    print("Veuillez entrer des lettres valides")
+    exit()
