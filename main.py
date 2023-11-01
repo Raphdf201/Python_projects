58e6786763f72a295104dcb41c4480516a534c49
Raphdf201 <raphdf201@hotmail.com>
2023-11-01 09:33:04 -0400

    correction de main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from Calcultrice import launch_cal
-from Convertisseur import launch_cv
-from dé import dé
-from generateur_mdp import launch_mdp
-from manoir_hanté import manoirHanté
-from tests import launch_tests
-from x3 import x3
+import Calcultrice
+import Convertisseur
+import dé
+import generateur_mdp
+import manoir_hanté
+import x3
 
